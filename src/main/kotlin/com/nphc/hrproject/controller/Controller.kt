63ce08402ca34a8d4b1a00d08e670e8b93f04494
rushikeshnakhate <rushikeshnakhate@gmail.com>
2021-06-24 07:26:19 +0800

    added curd ops

--- src/main/kotlin/com/nphc/hrproject/controller/Controller.kt
+++ src/main/kotlin/com/nphc/hrproject/controller/Controller.kt
@@ -1,20 +1,28 @@
 package com.nphc.hrproject.controller
 
 import com.nphc.hrproject.model.EmployeeMasterData
+import com.nphc.hrproject.service.CSVService
+import com.nphc.hrproject.service.EmployeeService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
 class EmployeeController {
+    @Autowired
+    lateinit var csvService: CSVService
+
+    @Autowired
+    lateinit var empService: EmployeeService
 
     @PostMapping("/users")
     fun createEmployee(@Validated @RequestBody employeeMasterData: EmployeeMasterData) =
-        TODO()
+        empService.createEmployee(employeeMasterData)
 
-
+    //CSV upload
     @PostMapping("upload/file")
-    fun uploadCsvFIle(@Validated @RequestParam("file") file: MultipartFile) = TODO()
+    fun uploadCsvFIle(@Validated @RequestParam("file") file: MultipartFile) = csvService.uploadCsvFile(file)
 
     //Update Employee
     @PutMapping("/users/{id}")
@@ -22,26 +30,26 @@ class EmployeeController {
         @Validated
         @PathVariable("id") id: String,
         @RequestBody employeeMasterData: EmployeeMasterData
-    ) = TODO()
+    ) = empService.update(id, employeeMasterData)
 
 
     //Get Employee Data based on Filter
     @GetMapping("/users")
     fun getEmployees(
         @Validated
-        @RequestParam("minSalary") minSalary: Double,
-        @RequestParam("maxSalary") maxSalary: Double
-    ) = TODO()
+        @RequestParam("minSalary", required = false) minSalary: Double,
+        @RequestParam("maxSalary", required = false) maxSalary: Double
+    ) = empService.getEmployees(minSalary, maxSalary)
 
     //Get Employee All Employees
     @GetMapping("/users-all")
-    fun getAllEmployees() = TODO()
+    fun getAllEmployees() = empService.getAllEmployees()
 
     //Get data for specific employee
     @GetMapping("/users/{id}")
-    fun getEmployee(@Validated @PathVariable("id") id: String) = TODO()
+    fun getEmployee(@Validated @PathVariable("id") id: String) = empService.getEmployee(id)
 
     //Delete Record
     @DeleteMapping("/users/{id}")
-    fun deleteEmployee(@Validated @PathVariable("id") id: String) = TODO()
+    fun deleteEmployee(@Validated @PathVariable("id") id: String) = empService.deleteEmployee(id)
 }
--- src/main/kotlin/com/nphc/hrproject/exception/ExceptionControllerAdvice.kt
+++ src/main/kotlin/com/nphc/hrproject/exception/ExceptionControllerAdvice.kt
@@ -1,9 +1,38 @@
 package com.nphc.hrproject.exception
 
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice
 class ExceptionControllerAdvice : ResponseEntityExceptionHandler() {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        val errors = ex.bindingResult.fieldErrors.map { e -> "${e.field} : ${e.defaultMessage}" }.toList()
+        val apiError = ApiError(HttpStatus.BAD_REQUEST, errors)
+        return super.handleExceptionInternal(ex, apiError, headers, status, request)
+    }
 
+    @ExceptionHandler(InvalidInputException::class)
+    fun handleInvalidInput(invalidEmployeeIdException: InvalidInputException): ResponseEntity<String> {
+        return ResponseEntity<String>("Invalid input data", HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(NoSuchFieldException::class)
+    fun unexpectedElement(noSuchFieldException: NoSuchFieldException): ResponseEntity<String> {
+        return ResponseEntity<String>("Bad Input", HttpStatus.NOT_FOUND)
+    }
+
+    data class ApiError(val status: HttpStatus, val error: List<String>)
 }
